340b63b3eda6afafa5e75a7efeb8e7070cfb86b7
ljh <1182552084@qq.com>
2025-08-06 00:36:15 +0800

    0806项目构建

---
+++ MeiTuan-common/src/main/java/fun/roehui/enumeration/OperationType.java
@@ -0,0 +1,14 @@
+package fun.roehui.enumeration;
+
+public enum OperationType {
+
+    /**
+     * 更新操作
+     */
+    UPDATE,
+
+    /**
+     * 插入操作
+     */
+    INSERT
+}
---
+++ MeiTuan-common/src/main/java/fun/roehui/json/JacksonObjectMapper.java
@@ -0,0 +1,6 @@
+package fun.roehui.json;
+
+public class JacksonObjectMapper {
+    
+}
+
